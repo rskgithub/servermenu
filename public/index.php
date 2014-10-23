b90e137230766c6ef923e54e387f9484861b2bd7
Simon <simon@fredsted.me>
2014-10-23 19:30:30 +0200

    Clean up index.php

--- public/index.php
+++ public/index.php
@@ -3,52 +3,70 @@ require '../vendor/autoload.php';
 
 // Prepare app
 $app = new \Slim\Slim(array(
-    'templates.path' => '../templates',
+        'templates.path' => '../templates',
 ));
 
 // Prepare view
 $app->view(new \Slim\Views\Twig());
 $app->view->parserOptions = array(
-    'charset' => 'utf-8',
-    'cache' => realpath('../templates/cache'),
-    'auto_reload' => true,
-    'strict_variables' => false,
-    'autoescape' => true
+        'charset'          => 'utf-8',
+        'cache'            => realpath('../templates/cache'),
+        'auto_reload'      => true,
+        'strict_variables' => false,
+        'autoescape'       => true
 );
 
 $app->view->parserExtensions = array(
-    new \Slim\Views\TwigExtension(),
-    new \JSW\Twig\TwigExtension());
+        new \Slim\Views\TwigExtension(),
+        new \JSW\Twig\TwigExtension()
+);
+
+// Define 404 template
+$app->notFound(function () use ($app) {
+        $app->render('404.html.twig');
+});
 
 // Add configuration to application
 $config = include('../app/config.php');
 $app->config('s', $config);
 
+/*
+ * Views/Pages
+ */
+
 // Front page
-$app->get('/', function () use ($app) {
-    $app->render('index.html.twig');
+$app->get('/', function () use ($app, $config) {
+        $template_variables = array(
+                'config' => $config
+        );
+
+        $app->render('index.html.twig', $template_variables);
 });
+
 
 /*
  * JSON API Actions
  */
 
 // API middleware
-function apiRequest() {
-	$app = \Slim\Slim::getInstance();
-	$app->view(new \JsonApiView());
-	$app->add(new \JsonApiMiddleware());
+function apiRequest()
+{
+        $app = \Slim\Slim::getInstance();
+        $app->view(new \JsonApiView());
+        $app->add(new \JsonApiMiddleware());
 }
 
 // Get list of services
 $app->get('/api/services', 'apiRequest', function () use ($app, $config) {
-	if (empty($config['services']))
-		$app->notFound();
+        if (empty($config['services'])) {
+                $app->notFound();
+        }
 
-	$app->render(200, array(
-		'services' => array_keys($config['services']),
-	));
+        $app->render(200, array(
+                'services' => array_keys($config['services']),
+        ));
 });
+
 
 /*
  * AJAX Actions
@@ -68,10 +86,7 @@ $app->get('/ajax/service/:serviceId', function ($serviceId) use ($app, $config) 
         $app->render('service.html.twig', $service->getTemplateData());
 });
 
-// Define 404 template
-$app->notFound(function () use ($app) {
-    $app->render('404.html.twig');
-});
+
 
 // Run app
 $app->run();
