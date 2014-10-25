74c6c82a8dfa4142efb70564610fb0f66d176680
Simon <simon@fredsted.me>
2014-10-25 16:19:39 +0200

    Add initial JS code for javascript ajax refresh

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,34 @@
 jQuery( document ).ready(function( $ ) {
-    //TODO do something or delete this.
+    jQuery.fn.extend({
+	loading: function() {
+	    return this.each(function() {
+		$(this).addClass('data-loading');
+	    });
+	},
+	finished: function() {
+	    return this.each(function() {
+		$(this).removeClass('data-loading');
+	    })
+	}
+    });
+
+    // Handle AJAX functionality
+
+    $(".data-load").each(function(){
+	$(this).loading();
+	$(this).load("/ajax/"+$(this).data('loadtype')+"/"+$(this).data('id'), function(e,s,j){
+	    if (s == 'error')
+	    	$(this).removeClass('data-reload');
+	});
+	$(this).finished();
+    });
+
+    setInterval(function() {
+	$(".data-reload").each(function() {
+	    $(this).loading();
+	    $(this).load("/ajax/"+$(this).data('loadtype')+"/"+$(this).data('id'));
+	    $(this).finished();
+	})
+    }, 5000);
+
 });
