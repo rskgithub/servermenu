70f82c7e4017c51c507dd96515cf1ca43c36fe28
Simon <simon@fredsted.me>
2014-10-26 20:46:39 +0100

    Fix AJAX loader

--- public/js/app.js
+++ public/js/app.js
@@ -16,10 +16,12 @@ jQuery( document ).ready(function( $ ) {
 
     $(".data-load").each(function(){
 	$(this).loading();
-	$(this).load("/ajax/"+$(this).data('loadtype')+"/"+$(this).data('id'), function(e,s,j){
+	var url = "/ajax/"+$(this).data('loadtype')+"/"+$(this).data('loadid');
+	$(this).load(url, function(e,s,j){
 	    if (s == 'error') {
 		// Don't keep reloading items that don't work.
 		$(this).removeClass('data-reload');
+		$(this).append('[Error loading '+$(this).data('loadtype')+'/'+$(this).data('loadid')+']');
 	    } else {
 		$(this).finished();
 		$(".ttip").tooltip();
@@ -29,12 +31,16 @@ jQuery( document ).ready(function( $ ) {
 
     setInterval(function() {
 	$(".data-reload").each(function() {
-	    $(this).load("/ajax/"+$(this).data('loadtype')+"/"+$(this).data('id'), function(e,s,j){
+	    $(this).load("/ajax/"+$(this).data('loadtype')+"/"+$(this).data('loadid'), function(e,s,j){
 		if (s != 'error') {
 		    $(".ttip").tooltip();
 		}
 	    });
 	})
     }, 5000);
+
+    // Miscellaneous UI stuff
+
+    $("a[data-id=feed-0]").tab('show'); // Load first feed tab
 
 });
