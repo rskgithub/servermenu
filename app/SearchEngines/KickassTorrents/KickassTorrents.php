e704d669f44035f60a4767eb90b686947bb95a63
Simon Fredsted <simon@fredsted.me>
2014-12-14 16:53:50 +0100

    Add KickassTorrents SearchEngine

---
+++ app/SearchEngines/KickassTorrents/KickassTorrents.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: simon
+ * Date: 30/10/14
+ * Time: 21:45
+ */
+
+namespace ServerMenu\SearchEngines\KickassTorrents;
+
+
+use ServerMenu\Receiver;
+use ServerMenu\SearchEngine;
+
+class KickassTorrents extends SearchEngine {
+
+	use Receiver;
+
+	/**
+	 * @return array
+	 */
+	protected function getReceiverTypes()
+	{
+		return array("search");
+	}
+
+	/**
+	 * Return array with search results and Service senders.
+	 *
+	 * @param string $searchQuery
+	 * @param int $amount
+	 * @param int $beginAt
+	 * @throws \Exception
+	 * @return mixed
+	 */
+	public function getTemplateData($searchQuery, $amount = self::DEFAULT_AMOUNT, $beginAt = 0)
+	{
+		$url = "https://kickass.so";
+		$urlenc_term = urlencode($searchQuery);
+		$raw = file_get_contents("$url/usearch/$urlenc_term/");
+
+		$html = new \simple_html_dom();
+		$html->load(gzdecode($raw));
+
+		$results = array();
+
+		foreach($html->find('table.data tbody tr') as $row) {
+			$title = $row->find("td",0);
+			$size = $row->find("td",1);
+			if (!isset($size))
+				continue;
+
+
+			$results[] = array(
+				'title' => $title->find("a", 7)->plaintext,
+				'link' => $url . $title->find("a", 0)->attr['href'],
+				'subtitle' => "seeds: {$row->find("td",4)->plaintext}, leeches: {$row->find("td",5)->plaintext}",
+				'size' => $size->innertext,
+				'date' => $row->find("td",3)->plaintext,
+				'actions' => array(
+					array(
+						'pluginType' => 'Services',
+						'receiverType' => 'magnet',
+						'content' => $title->find("a", 3)->attr['href'],
+						'glyphicon' => 'download',
+						'title' => 'Download'
+					)
+				)
+			);
+		}
+
+		return array('results'=>$results);
+	}
+
+	/**
+	 * The plugin must implement this method to safely be able to
+	 * receive content.
+	 *
+	 * @param $receiverType
+	 * @param $content
+	 * @return mixed
+	 */
+	public function receiveContent($receiverType, $content)
+	{
+		return $this->getTemplateData($content);
+	}
+
+} 
